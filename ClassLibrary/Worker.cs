08799eb540cccb5e4c18315f3ba22ea2cc17401b
Dunge <dunge2@yahoo.com>
2023-03-01 00:50:14 -0500

    Fix directory structure

---
+++ ClassLibrary/Worker.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ClassLibrary
+{
+    public class Worker : BackgroundService
+    {
+        public Worker(ILogger<Worker> logger)
+        {
+            logger.LogInformation("Worker console logging - change me");
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
